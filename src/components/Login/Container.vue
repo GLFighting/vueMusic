b0c4aad36164914c96c27b4d0620236380d04211
GL <1137559326@qq.com>
2022-03-08 09:40:26 +0800

    git commit -m 'add login-container slot'

---
+++ src/components/Login/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+  #login-container{
+    width: 80%;
+    height: 100%;
+    background-color: rgba(175, 170, 170, 0.733);
+  }
+</style>
