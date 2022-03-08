4e217a6e6c7cda0393cf09b888b81a9431326f02
GL <1137559326@qq.com>
2022-03-08 11:53:44 +0800

    git commit -m 'import topbar'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
   import MainTabBar from './views/MainTabBar.vue'  //导入底部导航栏组件
+  import TopBar from './components/TopBar/TopBar.vue'
 </script>
 
 <template>
+  <TopBar></TopBar>
   <router-view></router-view>
   <MainTabBar></MainTabBar>
 </template>
